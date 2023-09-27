67a56c0f703e21b452435d55588a6064ae06bbba
Vaiizz <felipeflores3108@gmail.com>
2023-09-26 23:56:48 -0300

    decimo

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -97,3 +97,11 @@ BEGIN
     SELECT * FROM Autor;
 END //
 DELIMITER ;
+
+DELIMITER //
+SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS Autor
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
